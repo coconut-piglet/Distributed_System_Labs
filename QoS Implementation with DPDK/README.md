6c2e04625c9248c99ace2a684894f518261c6cc7
YUENIIMAA <yueniimaa@outlook.com>
2020-04-06 10:17:38 +0800

    add missing header to qos.h

--- QoS Implementation with DPDK/README.md
+++ QoS Implementation with DPDK/README.md
@@ -1,3 +1,16 @@
 # QoS Implementation with DPDK
 
-施工中...
+## Task 1A  Implement a meter
+
+meter使用单速率三颜色标记\(srTCM\)，工作在色盲模式下，关于srTCM有三个参数：
+
+* Committed Information Rate \(CIR\) : 提交信息率，表示每秒IP包地字节数
+
+* Committed Burst Size \(CBS\) : 提交Burst大小，以字节为单位
+* Excess Burst Size \(EBS\) : 超量Burst大小，以字节为单位
+
+不超过CBS标绿色，CBS到EBS之间标黄色，超过EBS标红色，色盲模式假定所有incoming packet无色。
+
+## Task 1B  Implement a dropper
+
+待施工
--- QoS Implementation with DPDK/qos.h
+++ QoS Implementation with DPDK/qos.h
@@ -1,13 +1,16 @@
 #ifndef __QOS_H__
 #define __QOS_H__
 
-#define APP_FLOWS_MAX       4
+#include <stdint.h>
+
+#define APP_FLOWS_MAX 4
 
 /**
  * Single-Rate Three Color Meter, blind mode
  */
 
-enum qos_color {
+enum qos_color
+{
     GREEN = 0,
     YELLOW,
     RED
@@ -19,8 +22,6 @@ int qos_meter_init(void);
 /* Meter pkt, return color*/
 enum qos_color qos_meter_run(uint32_t flow_id, uint32_t pkt_len, uint64_t time);
 
-
-
 /**
  * Weighted Random Early Detection, weighted by Color
  */
@@ -28,9 +29,7 @@ enum qos_color qos_meter_run(uint32_t flow_id, uint32_t pkt_len, uint64_t time);
 /* Init dropper */
 int qos_dropper_init(void);
 
-
 /* Make drop decision, 1-drop, 0-pass */
 int qos_dropper_run(uint32_t flow_id, enum qos_color color, uint64_t time);
-
 
 #endif
