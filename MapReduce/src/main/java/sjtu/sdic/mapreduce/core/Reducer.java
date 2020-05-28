ec53a689ac77b61fdcc605cb231056691cacf761
YUENIIMAA <yueniimaa@outlook.com>
2020-05-28 12:05:22 +0800

    implement doReduce

--- MapReduce/src/main/java/sjtu/sdic/mapreduce/core/Reducer.java
+++ MapReduce/src/main/java/sjtu/sdic/mapreduce/core/Reducer.java
@@ -1,18 +1,15 @@
 package sjtu.sdic.mapreduce.core;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import sjtu.sdic.mapreduce.common.KeyValue;
 import sjtu.sdic.mapreduce.common.Utils;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * Created by Cachhe on 2019/4/19.
@@ -58,6 +55,76 @@ public class Reducer {
      * @param reduceF user-defined reduce function
      */
     public static void doReduce(String jobName, int reduceTask, String outFile, int nMap, ReduceFunc reduceF) {
-        
+        /* to sort the intermediate key/value pairs by key, use hashmap */
+        Map<String, List<String>> sortedKeyValues = new HashMap<>();
+
+        for (int i = 0; i < nMap; i++) {
+            InputStream inputStream = null;
+            try {
+                /* read data from intermediate file */
+                inputStream = new FileInputStream(Utils.reduceName(jobName, i, reduceTask));
+                int length = inputStream.available();
+                byte[] data = new byte[length];
+                inputStream.read(data);
+
+                /* extract intermediate key/value pairs */
+                String interContents = new String(data);
+                List<KeyValue> keyValuesSrc = JSON.parseArray(interContents, KeyValue.class);
+
+                /* sort extracted intermediate key/value pairs */
+                for (KeyValue keyValue : keyValuesSrc) {
+                    if (sortedKeyValues.containsKey(keyValue.key)) {
+                        sortedKeyValues.get(keyValue.key).add(keyValue.value);
+                    } else {
+                        List<String> values = new ArrayList<String>() {{add(keyValue.value);}};
+                        sortedKeyValues.put(keyValue.key, values);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("[error] failed to read interFile");
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("[error] failed to close interFile");
+                    }
+                } else {
+                    System.out.println("[info] doReduce failed");
+                }
+            }
+        }
+
+        /* perform user-defined reduce function on each key */
+        JSONObject keyValuesDst = new JSONObject();
+        for (String key : sortedKeyValues.keySet()) {
+            String[] values = sortedKeyValues.get(key).toArray(new String[0]);
+            String reducedValue = reduceF.reduce(key, values);
+            keyValuesDst.put(key, reducedValue);
+        }
+
+        /* write reduce output to outFile */
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(outFile);
+            byte[] outContents = keyValuesDst.toString().getBytes();
+            outputStream.write(outContents);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("[error] failed to write outFile");
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("[error] failed to close outFile");
+                }
+            } else {
+                System.out.println("[info] doReduce failed");
+            }
+        }
     }
 }
