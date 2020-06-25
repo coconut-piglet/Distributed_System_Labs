ed4202bc0b1ae3ba7ee893218cbc4bb97c496783
YUENIIMAA <yueniimaa@outlook.com>
2020-06-25 17:08:59 +0800

    add user interface and implement NULL/INVALID/HELP/EXIT operation

--- Distributed Key-Value Storage System/sourcecode/src/main/java/client/kvClient.java
+++ Distributed Key-Value Storage System/sourcecode/src/main/java/client/kvClient.java
@@ -1,4 +1,86 @@
 package client;
 
+import java.util.Scanner;
+
+/*
+ * Client of Distributed Key-Value Storage System
+ * TODO:
+ *   [√] add user interface
+ *   [√] support command NULL
+ *   [√] support command INVALID
+ *   [√] support command HELP
+ *   [√] support command EXIT
+ *   [ ] support RPC
+ *   [ ] support command PUT
+ *   [ ] support command READ
+ *   [ ] suppoer command DELETE
+ */
 public class kvClient {
+    private static void printUsage() {
+        System.out.println("usage:");
+        System.out.println("\thelp               display helper information");
+        System.out.println("\tput [key] [value]  create or update data");
+        System.out.println("\tread [key]         read data from cluster");
+        System.out.println("\tdelete [key]       delete data from cluster");
+        System.out.println("\texit               exit kvClient");
+    }
+
+    private static void printMessage(String msg) {
+        System.out.println("kvClient: " + msg);
+    }
+
+    public static void main(String[] argv) {
+        /* print information */
+        System.out.println(".d8888. d88888b d8888b.   j88D  d88888D      db   dD db    db  .o88b. db      d888888b d88888b d8b   db d888888b ");
+        System.out.println("88'  YP 88'     VP  `8D  j8~88  VP  d8'      88 ,8P' 88    88 d8P  Y8 88        `88'   88'     888o  88 `~~88~~' ");
+        System.out.println("`8bo.   88ooooo   oooY' j8' 88     d8'       88,8P   Y8    8P 8P      88         88    88ooooo 88V8o 88    88    ");
+        System.out.println("  `Y8b. 88~~~~~   ~~~b. V88888D   d8'        88`8b   `8b  d8' 8b      88         88    88~~~~~ 88 V8o88    88    ");
+        System.out.println("db   8D 88.     db   8D     88   d8'         88 `88.  `8bd8'  Y8b  d8 88booo.   .88.   88.     88  V888    88    ");
+        System.out.println("`8888Y' Y88888P Y8888P'     VP  d8'          YP   YD    YP     `Y88P' Y88888P Y888888P Y88888P VP   V8P    YP    ");
+        System.out.println("_________________________________________________________________________________________________________________");
+        System.out.println("Welcome To Distributed Key-Value Storage System By YUEQI ZHAO");
+
+        /* start routine */
+        while (true) {
+            System.out.print("kvClient > ");
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine().trim();
+            String args[] = cmd.split(" ");
+
+            /* empty input or HELP */
+            if (cmd.equals("") || args[0].equals("help")) {
+
+                /* display how to use this program */
+                printUsage();
+
+            }
+
+            /* PUT operation */
+            else if (args[0].equals("put")) {}
+
+            /* READ operation */
+            else if (args[0].equals("read")) {}
+
+            /* DELETE operation */
+            else if (args[0].equals("delete")) {}
+
+            /* EXIT operatoin */
+            else if (args[0].equals("exit")) {
+
+                /* say goodbye and end looping */
+                printMessage("goodbye");
+                break;
+
+            }
+
+            /* INVALID operation */
+            else {
+
+                /* alert and ask user to see usage (git style) */
+                printMessage("'" + args[0] + "' is not a kvClient command, see 'help' for usage");
+
+            }
+        }
+    }
 }
+
